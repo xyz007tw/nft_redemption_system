74b9210ba894528b6f3943413a4f35a6db23b035
xyz007tw <xyz007tw@github.com>
2026-09-20 18:10:58 +0800

    chore: disable strict eslint/ts checks during build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@wagmi/connectors'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
